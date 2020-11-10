0e56c2a44ef9a1de4223fbcbd47e6cd91d98b304
Rod-S <rodsanati@gmail.com>
2020-11-10 14:38:48 -0600

    aphorisms request working correctly

--- js/script.js
+++ js/script.js
@@ -1,122 +1,54 @@
 (() => {
     // event listener to respond to "Show another quote" button clicks
     // when user clicks anywhere on the button, the "combineFunction" function is called
-    document.getElementById('loadQuote').addEventListener("click", combineFunction, false);
-
-    // function retrieveAphorisms() {
-    //
-        // Creating the XMLHttpRequest object
-    //     var request = new XMLHttpRequest();
-    //
-    //     // Instantiating the request object
-    //     request.open("GET", "http://aphorisms.glitch.me/api/all", true);
-    //
-    //     // Defining event listener for readystatechange event
-    //     request.onreadystatechange = function() {
-    //
-    //         // Check if the request is compete and was successful
-    //         if (this.readyState === 4 && this.status === 200) {
-    //
-    //             // Inserting the response from server into an HTML element
-    //             console.log(request.responseText)
-    //         }
-    //         // Sending the request to the server
-    //         request.send();
-    //     }
-    // }
-    // retrieveAphorisms();
-        function httpGet(theUrl)
-        {
-            var xmlHttp = new XMLHttpRequest();
-            xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-            xmlHttp.send( null );
-            console.log(xmlHttp.responseText);
-            return xmlHttp.responseText;
-        }
-        httpGet("http://aphorisms.glitch.me/api/all");
+    // document.getElementById('loadQuote').addEventListener("click", combineFunction, false);
 
     //array of quote objects
-    var quotes = [
-        {
-            quote: "Do or do not. There is no try.",
-            source: "Yoda",
-            citation: "Star Wars Episode V: The Empire Strikes Back",
-            year: 1980,
-            category: "Movies"
-        },
-        {
-            quote: "The needs of the many outweigh the needs of the few, or the one.",
-            source: "Spock",
-            citation: "Star Trek II: The Wrath of Khan",
-            year: 1982,
-            category: "Movies"
-        },
-        {
-            quote: "Leave nothing for tomorrow which can be done today.",
-            source: "Abraham Lincoln",
-            citation: "Memorandum for Law Lecture",
-            year: 1850,
-            category: "Politics"
-        },
-        {
-            quote: "Float like a butterfly, sting like a bee.",
-            source: "Muhammad Ali",
-            citation: "The Rumble in the Jungle",
-            year: 1974,
-            category: "Sports"
-        },
-        {
-            quote: "One is never afraid of the unknown; one is afraid of the known coming to an end.",
-            source: "Jiddu Krishnamurti",
-            category: "philosophy"
-        },
-    ];
+    var quotes;
 
-    //return a random object from quotes array
-    function getRandomQuote() {
-        var object = Math.floor(Math.random() * quotes.length);
-        return quotes[object];
-    }
+    $.ajax("http://aphorisms.glitch.me/api/all").done(function(data, status, jqXhr) {
+        var quotes = data;
+        quotes.push(data);
+        console.log(quotes);
+        document.getElementById('loadQuote').addEventListener("click", combineFunction, false);
+        //return a random object from quotes array
+        function getRandomQuote() {
+            var object = Math.floor(Math.random() * quotes.length);
+            console.log(quotes[object]);
+            return quotes[object];
+        }
 
-    //call getRandomQuote function, concatenate HTML string and print quote to the page
-    function printQuote() {
-        var quoteHTML;
-        var outputDiv;
-        var randomObject = getRandomQuote();
-        //conditional added for quote object lacking year or citation properties
-        if (!randomObject.year || !randomObject.citation) {
-            quoteHTML = '<p class="quote">' + randomObject.quote + '</p>';
-            quoteHTML += '<p class="source">' + randomObject.source + '';
-            quoteHTML += '<span class="category">' + randomObject.category + '</span> </p>';
-            outputDiv = document.getElementById('quote-box');
-            outputDiv.innerHTML = quoteHTML;
-        } else {
-            quoteHTML = '<p class="quote">' + randomObject.quote + '</p>';
-            quoteHTML += '<p class="source">' + randomObject.source + '';
-            quoteHTML += '<span class="citation">' + randomObject.citation + '</span>';
-            quoteHTML += '<span class="year">' + randomObject.year;
-            quoteHTML += '<span class="category">' + randomObject.category + '</span> </p>';
+        //call getRandomQuote function, concatenate HTML string and print quote to the page
+        function printQuote() {
+            var quoteHTML;
+            var outputDiv;
+            var randomObject = getRandomQuote();
+            //conditional added for quote object lacking year or citation properties
+            quoteHTML = `<p class="quote">${randomObject.quote}</p>
+                         <p class="source">${randomObject.author}</p>`
             outputDiv = document.getElementById('quote-box');
             outputDiv.innerHTML = quoteHTML;
         }
-    }
 
-    //randomize RGB values to override styles.css body background-color key-value pair
-    function getRandomColor() {
-        let red = Math.floor(Math.random() * 256);
-        let green = Math.floor(Math.random() * 256);
-        let blue = Math.floor(Math.random() * 256);
-        document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-    }
+        //randomize RGB values to override styles.css body background-color key-value pair
+        function getRandomColor() {
+            let red = Math.floor(Math.random() * 256);
+            let green = Math.floor(Math.random() * 256);
+            let blue = Math.floor(Math.random() * 256);
+            document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        }
 
-    // 5 second timer to randomly generate quote and background color
-    var quoteInterval = window.setInterval(combineFunction, 5000);
+        // 5 second timer to randomly generate quote and background color
+        var quoteInterval = window.setInterval(combineFunction, 5000);
 
-    //combined function for use with eventListener()
-    function combineFunction() {
-        getRandomColor();
-        printQuote();
-        clearInterval(quoteInterval);
-        quoteInterval = window.setInterval(combineFunction, 5000);
-    }
+        //combined function for use with eventListener()
+        function combineFunction() {
+            getRandomColor();
+            printQuote();
+            clearInterval(quoteInterval);
+            quoteInterval = window.setInterval(combineFunction, 5000);
+        }
+    })
+
+
 })();
